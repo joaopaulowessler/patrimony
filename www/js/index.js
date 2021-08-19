1b14954c194f9ce17eb41cd175b3da886229bf44
Joao Wessler <joaopaulowessler@gmail.com>
2021-08-19 12:52:34 -0300

    teste inicial

---
+++ www/js/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+
+    //alert("alo");
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    //document.getElementById('deviceready').classList.add('ready');
+}
+
+function sincronizar() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda=%27USD%27&@dataInicial=%2708-15-2021%27&@dataFinalCotacao=%2708-18-2021%27&format=json", false );
+    xmlHttp.send(null);
+
+    alert(xmlHttp.responseText);
+}
